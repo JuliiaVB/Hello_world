6d7117fe4117dc5a9084247e99e40d06b1a1868e
JuliiaVB <106270625+JuliiaVB@users.noreply.github.com>
2023-07-01 23:02:19 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html class="1-html" lang="ru">
+<head> 
+    <title>Задание 1</title>
+    <link rel="stylesheet" href="css_main.css" >
+</head>
+<body>
+     <div class='div_center'>
+          <h1>Hello, world!</h1>
+      <p1>
+      <?php
+        echo date("D F j, Y")
+        ?>
+      </p1>
+     </div>
+</body>
+
+</html>
+
+ 
